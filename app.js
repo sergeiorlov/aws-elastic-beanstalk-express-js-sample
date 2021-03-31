493ad6ba780cd6f85ec31767cbbb957d40239a2f
SergeiOrlov <kalinrachev@gmail.com>
2021-03-31 18:25:18 +0100

    change message again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, yay world!'));
+app.get('/', (req, res) => res.send('Hello, hello, hello, you world!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
